a281897436e284b5dbc02abca1e87c2e34a7e6f4
黃煒翔 <silverknight3x@gmail.com>
2017-12-07 16:33:53 +0800

    book

---
+++ PCC/src/main/java/spring/SpringMvcConfigRoom.java
@@ -0,0 +1,56 @@
+package spring;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.web.context.support.ServletContextResource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+import org.springframework.web.servlet.view.XmlViewResolver;
+
+@Configuration
+@ComponentScan(basePackages= {"controller.room"})
+@EnableWebMvc
+public class SpringMvcConfigRoom extends AbstractAnnotationConfigDispatcherServletInitializer implements WebMvcConfigurer {
+	
+	@Autowired
+	private ServletContext application;
+	
+	@Override
+	public void configureViewResolvers(ViewResolverRegistry registry) {
+		XmlViewResolver XmlViewResolver = new XmlViewResolver();
+		Resource resource = new ServletContextResource(application, "/WEB-INF/roomViews.xml");
+		XmlViewResolver.setLocation(resource);
+		registry.viewResolver(XmlViewResolver);
+	}
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {SpringMvcConfigRoom.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"*.room"};
+	}
+
+	@Override
+	protected String getServletName() {
+		return "springMvcConfigRoom";
+	}
+	
+	
+	
+
+}
