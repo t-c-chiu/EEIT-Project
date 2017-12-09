a94b88b47e2b12896625170d072e6c56e40e84a9
User <User@User-PC>
2017-12-09 15:52:18 +0800

    room calendar

--- PCC/src/main/java/controller/room/RoomReservationController.java
+++ PCC/src/main/java/controller/room/RoomReservationController.java
@@ -30,7 +30,8 @@ public class RoomReservationController {
 	@InitBinder
 	public void intialize(WebDataBinder webDataBinder) {
 		webDataBinder.registerCustomEditor(java.util.Date.class,
-				new CustomDateEditor(new SimpleDateFormat("MM/dd/yyyy"), true));
+//				new CustomDateEditor(new SimpleDateFormat("MM/dd/yyyy"), true));
+				new CustomDateEditor(new SimpleDateFormat("yyyy/MM/dd"), true));
 	}
 
 	@RequestMapping(path = "/reserve.room", method = RequestMethod.POST)
