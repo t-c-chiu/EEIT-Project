08e7f9e1277a04f930cc447c94d79839c81130d7
毛政尹 <eeit9825@gmail.com>
2017-12-13 17:49:15 +0800

    servantList finish
    prepare for 彈出視窗,排版

--- PCC/src/main/java/controller/match/MatchController.java
+++ PCC/src/main/java/controller/match/MatchController.java
@@ -1,9 +1,32 @@
 package controller.match;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import model.service.ServantService;
 
 @Controller
+@SessionAttributes({"servantList"})
 public class MatchController {
+    @Autowired
+    public ServantService service;
 
+    @RequestMapping(path="/test.match", method = RequestMethod.GET)   
+    public String method(Model model) {
+    	
+    	List<List> servantList = service.showServantList();
+//    	Map<String, String> testMap = new HashMap<String, String>();
+//    	testMap.put("xxx", sdf);
 
+    	
+        
+    	model.addAttribute("servantList", servantList);
+    	return "show";
+    }
 }
--- PCC/src/main/java/spring/SpringMvcConfigMatch.java
+++ PCC/src/main/java/spring/SpringMvcConfigMatch.java
@@ -7,7 +7,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
 import org.springframework.web.context.support.ServletContextResource;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
